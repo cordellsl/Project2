b4acb113011fe3b9828f10ef7d239f75b43e4156
Cordell <cordellsl@yahoo.com>
2019-07-17 19:40:19 -0400

    working mapper function and main for problem 1

---
+++ main1.java
@@ -0,0 +1,59 @@
+package com.revature;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import com.revature.map.Mapper1;
+import com.revature.reduce.Reducer1;
+import com.revature.reduce.Combiner1;
+
+public class main {
+	
+	public static void main(String[] args) throws Exception {
+		
+		if(args.length != 2) {
+			System.out.println("WordCount usage: <input dir> <output dir>");
+			System.exit(-1);
+		}
+		
+		Job job = new Job();
+		
+		// Telling the job which class is the driver
+		job.setJarByClass(main.class);
+		
+		job.setJobName("Problem 1");
+		
+		// Input and output paths
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		
+		// Specify the Mapper and Reducer class 
+		job.setMapperClass(Mapper1.class);
+		
+		//job.setReducerClass(SumReducer.class);
+		//job.setReducerClass(Reducer1.class);
+		
+		//job.setCombinerClass(Combiner1.class);
+		
+		
+		
+		job.setNumReduceTasks(0);
+		
+		// Specify the types of the final output
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(DoubleWritable.class);
+		
+		// Check if job completed
+		boolean success = job.waitForCompletion(true);
+
+		System.exit(success ? 0 : 1);
+		
+	}
+
+}
